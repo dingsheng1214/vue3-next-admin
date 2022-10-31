207b73a5c9641ea3e6b0cc4f7961f86dbfcd4d49
dingsheng <dingduoduo0916@gmail.com>
2022-10-31 16:22:30 +0800

    style(custom): login UI完善

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-    <el-form>
+    <el-form class="login-form">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
@@ -26,29 +26,95 @@
         </span>
       </el-form-item>
 
-      <el-button type="primary">登录</el-button>
+      <el-button class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
   import { Avatar } from '@element-plus/icons-vue'
-  import { useRouter } from 'vue-router'
-  import { useUserStore } from '@/store'
-
-  const userStore = useUserStore()
-  const router = useRouter()
-  const handleLogin = () => {
-    userStore.setUserInfo({
-      userId: '1',
-      username: 'tony',
-      realName: 'tony',
-      avatar: 'avatar',
-    })
-    router.push({
-      path: '/home',
-    })
-  }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+  $bg: #2d3a4b;
+  $dark_gray: #889aa4;
+  $light_gray: #eee;
+  $cursor: #fff;
+
+  .login-container {
+    min-height: 100%;
+    width: 100%;
+    background-color: $bg;
+    overflow: hidden;
+
+    .login-form {
+      position: relative;
+      width: 520px;
+      max-width: 100%;
+      padding: 160px 35px 0;
+      margin: 0 auto;
+      overflow: hidden;
+
+      ::v-deep .el-form-item {
+        border: 1px solid rgba(255, 255, 255, 0.1);
+        background: rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        color: #454545;
+      }
+
+      ::v-deep .el-input {
+        display: inline-block;
+        height: 47px;
+        width: 85%;
+
+        .el-input__wrapper {
+          width: 100%;
+          background-color: transparent;
+          box-shadow: 0 0 0 1px transparent;
+        }
+        input {
+          background: transparent;
+          border: 0px;
+          border-radius: 0px;
+          padding: 12px 5px 12px 15px;
+          color: $light_gray;
+          height: 47px;
+          caret-color: $cursor;
+        }
+      }
+    }
+
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      display: inline-block;
+    }
+
+    .title-container {
+      position: relative;
+
+      .title {
+        font-size: 26px;
+        color: $light_gray;
+        margin: 0px auto 40px auto;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
+    }
+
+    .login-btn {
+      width: 100%;
+    }
+  }
+</style>
