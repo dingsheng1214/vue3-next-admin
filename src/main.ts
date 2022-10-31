2ebe245794d754be38f2b55d691a7b57e552bef0
dingsheng <dingduoduo0916@gmail.com>
2022-10-31 14:27:29 +0800

    feat(custom): 登录ui开发

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import './style.css'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <div>Login</div>
-    <button @click="handleLogin">登录</button>
+  <div class="login-container">
+    <el-form>
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password"></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+
+      <el-button type="primary">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { Avatar } from '@element-plus/icons-vue'
   import { useRouter } from 'vue-router'
   import { useUserStore } from '@/store'
 
